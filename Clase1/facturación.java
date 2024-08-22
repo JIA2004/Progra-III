9b59b0edf6aba6f56ea98bc294e91ad8c9f1a6bc
Juani Aguirre <juani.aguirremonti@gmail.com>
2024-08-22 20:14:50 -0300

    Carpeta creada

---
+++ Clase1/facturación.java
@@ -0,0 +1,28 @@
+public class facturación {
+    public static void main(String[] args) {
+        
+        int[][] facturas = {{123,53,4500},{124,47,5800},{125,67,7400},{126,87,3200}};
+
+        Cliente[] clientes = ;
+
+    }
+
+    public class Cliente {
+        private int id;
+        private String nombre;
+    
+        public Cliente(int id, String nombre) {
+            this.id = id;
+            this.nombre = nombre;
+        }
+    
+        public int getId() {
+            return id;
+        }
+    
+        public String getNombre() {
+            return nombre;
+        }
+    }
+    
+}
