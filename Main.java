e33217270e2fef3bfc311dd6d9a786d08c0f49ea
Juani Aguirre <juani.aguirremonti@gmail.com>
2024-08-22 18:43:26 -0300

    comentarios fixed

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ public class Main {
         int[] array = {1,43,12,44,60,65}; //1
         int maxi = 0; //1
 
-        for (int i : array) { //1 + 2(n+1) + n
+        for (int i : array) { //1 + n
             if  (i > maxi) { //n
                 maxi = i; //n
             }
         }
 
-        System.out.println(maxi); //1.
+        System.out.println(maxi); //1
 
     }
 }
