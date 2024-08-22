6285f68d8a5ee63a00fbb5d1b863726820ce74c9
faust <faustinofiordalisi@gmail.com>
2024-08-22 18:10:49 -0300

    comentarios

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {1,43,12,44,60,65};
-        int maxi = 0;
+        int[] array = {1,43,12,44,60,65}; //1
+        int maxi = 0; //1
 
-        for (int i : array) {
-            if  (i > maxi) {
-                maxi = i;
+        for (int i : array) { //1 + 2(n+1) + n
+            if  (i > maxi) { //n
+                maxi = i; //n
             }
         }
 
-        System.out.println(maxi);
+        System.out.println(maxi); //1
 
     }
 }
