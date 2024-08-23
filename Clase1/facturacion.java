625c18ed260cf4ffaf3924dc09f6fc88c4769304
JIA2004 <juani.aguirremonti@gmail.com>
2024-08-23 08:45:05 -0300

    corrección facturación

--- Clase1/facturacion.java
+++ Clase1/facturacion.java
@@ -3,60 +3,71 @@ import java.util.List;
 
 public class facturacion {
     public static void main(String[] args) {
-        int[][] listacomprobante={{54,36,3400},{55,12,4300},{56,25,2500}};
-        Cliente numero1=new Cliente(36, "Lucas");
-        Cliente numero2=new Cliente(12, "Franco");
-        Cliente numero3=new Cliente(25, "María");
+        int[][] documentos={{54,36,3400},{55,12,4300},{56,25,2500}};
+        Cliente cliente1=new Cliente(36, "Lucas");
+        Cliente cliente2=new Cliente(12, "Franco");
+        Cliente cliente3=new Cliente(25, "María");
 
-        List<Cliente> clientes=new ArrayList<>(); //creamos listas
-        clientes.add(numero1);
-        clientes.add(numero2);
-        clientes.add(numero3);
+        List<Cliente> listaClientes = new ArrayList<>(); //creamos listas
+        listaClientes.add(cliente1);
+        listaClientes.add(cliente2);
+        listaClientes.add(cliente3);
 
-        List<Resultado> resultados=calcularsumaimportes(listacomprobante,clientes);
-        for(Resultado Resultado: resultados){
-            System.out.println(Resultado);
+        List<Resumen> resumenes = calcularSuma(documentos, listaClientes);
+        for(Resumen resumen : resumenes){
+            System.out.println(resumen);
         }
     }
-    public static List<Resultado> calcularsumaimportes(int [][] comprobantes,List<Cliente> clientes){
-        List<Resultado> resultados=new ArrayList<>();
+    
+    public static List<Resumen> calcularSuma(int [][] documentos, List<Cliente> listaClientes){
+        List<Resumen> resumenes = new ArrayList<>();
 
-        for(Cliente cliente:clientes){
-            double sumaimportes=0.0;
-            for(int[] comprobante:comprobantes){
-                int idCliente=comprobante[1];
-                double importe=comprobante[2];
-                if (idCliente==cliente.getId()) {
-                    sumaimportes+=importe;
-                    
+        for(Cliente cliente : listaClientes){
+            double totalImportes = 0.0;
+            for(int[] documento : documentos){
+                int codigoCliente = documento[1];
+                double importe = documento[2];
+                if (codigoCliente == cliente.obtenerId()) {
+                    totalImportes += importe;
                 }
             }
-            resultados.add(new Resultado(cliente.getId(),cliente.getNombre() , sumaimportes));
+            resumenes.add(new Resumen(cliente.obtenerId(), cliente.obtenerNombre(), totalImportes));
         }
-        return resultados;
-    }}
-    class Cliente{
-        int id;
-        String nombre;
-        public Cliente(int id,String nombre){
-            this.id=id;
-            this.nombre=nombre;
-        }
-        public int getId() {
-            return id;
-        }
-        public String getNombre() {
-            return nombre;
-        }}
-    class Resultado {
-        private int idCliente;
-        private String nombreCliente;
-        private double sumaImportes;
+        return resumenes;
+    }
+}
 
-        public Resultado(int idCliente,String nombrecliente,double sumaimportes){
-            this.idCliente=idCliente;
-            this.nombreCliente=nombrecliente;
-            this.sumaImportes=sumaimportes;}
-        @Override
-        public String toString(){
-            return "resultado= idcliente= "+idCliente+", nombrecliente= "+nombreCliente+", sumaimportes= "+sumaImportes+" ";}}
+class Cliente {
+    int codigo;
+    String nombre;
+
+    public Cliente(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public int obtenerId() {
+        return codigo;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+}
+
+class Resumen {
+    private int codigoCliente;
+    private String nombreCliente;
+    private double totalImportes;
+
+    public Resumen(int codigoCliente, String nombreCliente, double totalImportes) {
+        this.codigoCliente = codigoCliente;
+        this.nombreCliente = nombreCliente;
+        this.totalImportes = totalImportes;
+    }
+
+    @Override
+    public String toString() {
+        return "Resumen= codigoCliente= " + codigoCliente + ", nombreCliente= " + nombreCliente + ", totalImportes= " + totalImportes;
+    }
+}
