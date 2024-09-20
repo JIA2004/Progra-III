e90e2d0cd4be6a1b23fcb214bf3858cbf2868b41
Juani Aguirre <juani.aguirremonti@gmail.com>
2024-09-19 22:06:32 -0300

    Actividad1 realizada

--- src/Clase4/Actividad1.java
+++ src/Clase4/Actividad1.java
@@ -1,5 +1,28 @@
 package Clase4;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Actividad1 {
-    
+    public static List<Integer> encontrarMinimoMonedas(int[] monedas, int monto) {
+        Arrays.sort(monedas);
+        List<Integer> resultado = new ArrayList<>();
+        
+        for (int i = monedas.length - 1; i >= 0; i--) {
+            while (monto >= monedas[i]) {
+                monto -= monedas[i];
+                resultado.add(monedas[i]);
+            }
+        }
+        
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        int[] monedas = {1, 5, 10, 25};
+        int monto = 36;
+        List<Integer> resultado = encontrarMinimoMonedas(monedas, monto);
+        System.out.println("Monedas usadas para hacer " + monto + ": " + resultado);
+    }
 }
